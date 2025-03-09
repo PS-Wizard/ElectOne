482ca95405f497e3b96dc9da81de949adc02175d
PS-Wizard <p.swoyam.1@gmail.com>
2025-03-09 22:00:26 +0545

    updated

--- backend/go.mod
+++ backend/go.mod
@@ -1,3 +1,7 @@
 module github.com/PS-Wizard/VotingSystem
 
 go 1.24.0
+
+require golang.org/x/crypto v0.36.0
+
+require github.com/a-h/templ v0.3.833 // indirect
--- backend/models/user.go
+++ backend/models/user.go
@@ -1,23 +1,73 @@
 package models
 
 import (
+	"crypto/rand"
+	"encoding/hex"
 	"fmt"
+	"net/http"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 type User struct {
 	VoterID  string `json:"voterID"`
-	password string
+	Password string
 }
 
-// TODO: Hash The Password
-var ListOfUsers = []User{
-	{
-		VoterID:  "123456",
-		password: "SomePassword",
-	},
+var ListOfUsers = []User{}
+
+func HashPassword(password string) (string, error) {
+	hashed, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(hashed), nil
 }
 
-func CreateUser(id, password string) {
-	fmt.Printf("Got %s, %s \n", id, password)
-	ListOfUsers = append(ListOfUsers, User{VoterID: id, password: password})
+func CreateUser(id, password string) error {
+	hashedPassword, err := HashPassword(password)
+	fmt.Println("Saved Hash Successfully :%s", hashedPassword)
+	if err != nil {
+		return err
+	}
+	ListOfUsers = append(ListOfUsers, User{VoterID: id, Password: hashedPassword})
+	return nil
+}
+
+func CheckCredentials(id, password string) bool {
+	for _, user := range ListOfUsers {
+		if user.VoterID == id {
+			return bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password)) == nil
+		}
+	}
+	return false
+}
+
+func GenerateSessionToken() string {
+	bytes := make([]byte, 16)
+	rand.Read(bytes)
+	return hex.EncodeToString(bytes)
+}
+
+func LoginUser(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, "Invalid Form", http.StatusBadRequest)
+		return
+	}
+
+	id := r.FormValue("voterid")
+	password := r.FormValue("password")
+
+	if CheckCredentials(id, password) {
+		token := GenerateSessionToken()
+		http.SetCookie(w, &http.Cookie{
+			Name:  "session",
+			Value: token,
+			Path:  "/",
+		})
+		fmt.Fprintf(w, "Login Successful")
+	} else {
+		http.Error(w, "Invalid Credentials", http.StatusUnauthorized)
+	}
 }
