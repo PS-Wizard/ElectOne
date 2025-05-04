8f24c1c4293db807febe1fc408651a2e6b81d5e6
Wizard <p.swoyam.1@gmail.com>
2025-05-04 14:32:06 +0545

    updated cors

--- backend/main.go
+++ backend/main.go
@@ -12,7 +12,8 @@ import (
 func main() {
 	app := fiber.New()
 	app.Use(cors.New(cors.Config{
-		AllowOrigins: "http://localhost:5173",
+		AllowOrigins: "*",
+		// AllowOrigins: "http://localhost:5173",
 		AllowHeaders: "*",
 		AllowMethods: "*",
 	}))
